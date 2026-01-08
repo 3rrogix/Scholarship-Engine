f3666d6297610a52b842592692f59a19bce71fee
3rrogix <p87hg4b67g@privaterelay.appleid.com>
2026-01-08 10:05:38 +0000

    actually implementing code meow

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,3 +1,19 @@
+
 document.getElementById('open-dashboard').addEventListener('click', () => {
   chrome.tabs.create({ url: chrome.runtime.getURL('dashboard.html') });
+});
+
+document.getElementById('import-links-google').addEventListener('click', () => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    const tab = tabs[0];
+    if (!tab || !tab.url.includes('google.com/search')) {
+      alert('Please focus a Google search results tab.');
+      return;
+    }
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ['content.js']
+    });
+    window.close();
+  });
 });
