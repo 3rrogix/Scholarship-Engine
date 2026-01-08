5bfb5b8ba2ef0dd1867a1fdf8f38380e9d817c52
3rrogix <p87hg4b67g@privaterelay.appleid.com>
2026-01-08 10:00:31 +0000

    actually implementing code meow

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,7 +1,7 @@
 // Content script to extract links from Google search results
-(() => {
+if (window.location.hostname === 'www.google.com' && window.location.pathname === '/search') {
   const links = Array.from(document.querySelectorAll('a'))
     .map(a => a.href)
     .filter(href => href && href.startsWith('http') && !href.includes('google.com'));
   chrome.runtime.sendMessage({ type: 'EXTRACTED_LINKS', links });
-})();
+}
